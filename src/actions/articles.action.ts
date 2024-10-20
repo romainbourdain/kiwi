50ce46cf10b367060c5173c7b364803b68e1fe23
Romain Bourdain <romain.bourdain93@gmail.com>
2024-10-20 18:41:00 +0200

    document article action

--- src/actions/articles.action.ts
+++ src/actions/articles.action.ts
@@ -12,6 +12,10 @@ import {
 } from "./articles.helper";
 
 const latestArticleSchema = z.object({ limit: z.number() });
+
+/**
+ * Fetches the latest articles based on a specified limit.
+ */
 export const getLatestArticles = authActionClient
   .schema(latestArticleSchema)
   .action(async ({ parsedInput: { limit } }) => {
@@ -19,6 +23,11 @@ export const getLatestArticles = authActionClient
   });
 
 const articleBySlugSchema = z.object({ slug: z.string() });
+
+/**
+ * Fetches an article by its slug and records a view for the current user.
+ * Also checks if the current user has liked the article.
+ */
 export const getArticleBySlug = authActionClient
   .schema(articleBySlugSchema)
   .action(async ({ parsedInput: { slug }, ctx: { user } }) => {
@@ -32,6 +41,12 @@ export const getArticleBySlug = authActionClient
   });
 
 const likeArticleSchema = z.object({ articleId: z.string() });
+
+/**
+ * Toggles the like status for a specific article based on the current user.
+ * If the article is already liked, the like is removed, otherwise a like is added.
+ * Also triggers revalidation of the article's related paths to update the cached content.
+ */
 export const likeArticle = authActionClient
   .schema(likeArticleSchema)
   .action(async ({ parsedInput: { articleId }, ctx: { user } }) => {
